682765d2e02c636e5cd1e725d0c8a8381b9f7d63
Niranjan <niranjancb2035@gmail.com>
2025-10-15 12:58:05 +0530

    made changes in frontend

--- client/src/lib/api-clients.js
+++ client/src/lib/api-clients.js
@@ -1,10 +1,16 @@
-
-import axios from 'axios';
-import { HOST } from '../utils/constants';
+import axios from "axios";
+import { HOST } from "../utils/constants";
 
 const apiClients = axios.create({
-    baseURL: HOST,
-    withCredentials:true
-})
+  baseURL: HOST,
+});
+
+apiClients.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token"); // stored after login
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default apiClients;
